af9a2d8bc13d801d63c384b811d55549a3638a22
nandakishore-dasari <nandudasari07@gmail.com>
2025-04-21 22:37:19 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const fetchTopHeadlines = async () => {
-  const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.API_KEY}`);
+  const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`);
   return res.data.articles;
 };
 
 const fetchByCategory = async (category) => {
-  const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.API_KEY}`);
+  const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`);
   return res.data.articles;
 };
 
